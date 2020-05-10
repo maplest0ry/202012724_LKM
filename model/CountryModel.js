5970ea01acee577853c32131fb86c391640be970
quokkaDeveloper <rbalsrbals94@gmail.com>
2020-05-10 23:10:23 +0900

    [added] Connection Setting

--- model/CountryModel.js
+++ model/CountryModel.js
@@ -1,69 +1,168 @@
 const fs = require('fs');
+const pool = require('./dbConnection');
+const {prepareTable} = require('./prepareTable');
+
 class Country {
+
     constructor() {
         const data = fs.readFileSync('./model/data.json');
         this.data = JSON.parse(data);
     }
 
-    showcountryList() {
-        if (this.data) {
-            return this.data;
+    showcountryList = async() => {
+        const sql = 'SELECT * FROM country';
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const [country, metadata] = await conn.query(sql);
+            return country;
+        } catch(error){
+            console.error(error);
+        } finally {
+            if(conn) {
+                conn.release();
+            }
         }
     }
-    detailCountry(id) {
-        return new Promise((resolve, reject) => {
-            for (var object of this.data) {
-                if (object.id == id) {
-                    resolve(object);
-                    return;
-                }
+
+    detailCountry = async(id) => {
+        const sql = 'SELECT * FROM country WHERE id = ?;';
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const [detail, metadata] = await conn.query(sql, id);
+            return detail[0];
+        } catch(error) {
+            console.error(error);
+        } finally {
+            if(conn){
+                conn.release();
             }
-            reject({ msg: _id + ' not found', code: 404 });
-        });
+        }
     }
 
-    addcountry(data) {
-        return new Promise((resolve, reject) => {
-            const idx = this.data[this.data.length - 1].id + 1;
-            const newcountry = {
-                id:idx,
-                name:data.name,
-                language:data.language,
-                capital:data.capital
+    addcountry = async (data) => {
+        const name = data.name
+        const language = data.language
+        const capital = data.capital
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            
+            // Insert
+            const sqlI = 'INSERT INTO country(name, language, capital) values (?, ?, ?);';
+            const data = [name, language, capital];
+            const resultI = await conn.query(sqlI, data);
+            
+            // Select
+            const sqlS = 'SELECT * FROM country WHERE id = ?;';
+            const resultS = await conn.query(sqlS, resultI[0].insertId);
+            return resultS[0];
+        } catch(error) {
+            console.error(error);
+        } finally {
+            if(conn) {
+                conn.release();
             }
-            this.data.push(newcountry);
-            resolve(newcountry);
-        });
+        }
     }
 
-    updatecountry(data){
-        return new Promise((resolve, reject) => {
-            for (var object of this.data) {
-                if (object.id == data.id) {
-                    object.name = data.name;
-                    object.language = data.language;
-                    object.capital = data.capital;
-                    console.log(object);
-                    resolve(object);
-                    return;
-                }
+    updatecountry = async(data) => {
+        const name = data.name;
+        const language = data.language;
+        const capital = data.capital;
+        const id = data.id
+        const sql = 'UPDATE country SET name = ?, language = ?, capital = ? WHERE id = ?;';
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            await conn.query(sql, [name, language, capital, id]);
+            return this.detailCountry(id);
+        } catch (error){
+            console.error(error);
+        } finally {
+            if(conn){
+                conn.release();
             }
-            reject({ msg: data.name + 'Fail', code: 404 });
-        });
+        }
     }
 
-    deletecountry(id){
-        return new Promise((resolve, reject) => {
-            for (var object of this.data) {
-                if (object.id == id) {
-                    this.data.splice(object.id,1);
-                    resolve(object.id," Deleted");
-                    return;
-                }
+    deletecountry = async (id) => {
+        const sql = 'DELETE FROM country WHERE id = ?;';
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const result = await conn.query(sql, [id]);
+            return null;
+        } catch(error){
+            console.error(error);
+        } finally {
+            if (conn){
+                conn.release();
             }
-            reject({ msg: id + 'Fail', code: 404 });
-        });
+        }
     }
+
+      // showcountryList() {
+    //     if (this.data) {
+    //         return this.data;
+    //     }
+    // }
+    
+    // detailCountry(id) {
+    //     return new Promise((resolve, reject) => {
+    //         for (var object of this.data) {
+    //             if (object.id == id) {
+    //                 resolve(object);
+    //                 return;
+    //             }
+    //         }
+    //         reject({ msg: _id + ' not found', code: 404 });
+    //     });
+    // }
+
+    // addcountry(data) {
+    //     return new Promise((resolve, reject) => {
+    //         const idx = this.data[this.data.length - 1].id + 1;
+    //         const newcountry = {
+    //             id:idx,
+    //             name:data.name,
+    //             language:data.language,
+    //             capital:data.capital
+    //         }
+    //         this.data.push(newcountry);
+    //         resolve(newcountry);
+    //     });
+    // }
+
+     // updatecountry(data){
+    //     return new Promise((resolve, reject) => {
+    //         for (var object of this.data) {
+    //             if (object.id == data.id) {
+    //                 object.name = data.name;
+    //                 object.language = data.language;
+    //                 object.capital = data.capital;
+    //                 console.log(object);
+    //                 resolve(object);
+    //                 return;
+    //             }
+    //         }
+    //         reject({ msg: data.name + 'Fail', code: 404 });
+    //     });
+    // }
+
+    // deletecountry(id){
+    //     return new Promise((resolve, reject) => {
+    //         for (var object of this.data) {
+    //             if (object.id == id) {
+    //                 this.data.splice(object.id,1);
+    //                 resolve(object.id," Deleted");
+    //                 return;
+    //             }
+    //         }
+    //         reject({ msg: id + 'Fail', code: 404 });
+    //     });
+    // }
 
 }
 module.exports = new Country();
--- router/CountryRouter.js
+++ router/CountryRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const countryModel = require('../model/CountryModel');
 
 
-router.get('/country', showcountryList);
-router.get('/country/:id',showCountryDetailView);
+// router.get('/country', showcountryList);
+// router.get('/country/:id',showCountryDetailView);
 router.get('/country/add',showCountryAddView);
 router.post('/country/add', addcountry);
 router.get('/country/edit/:id',showCountryEditView);
@@ -14,11 +14,45 @@ router.post('/country/delete/:id', deletecountry);
 
 module.exports = router;
 
-function showcountryList(req, res) {
-    const countryList = countryModel.showcountryList();
-    // const result = { data:countryList, count:countryList.length };
-    res.render('CountryListView',{ data:countryList, count:countryList.length });
-}
+// function showcountryList(req, res) {
+//     const countryList = countryModel.showcountryList();
+//     // const result = { data:countryList, count:countryList.length };
+//     res.render('CountryListView',{ data:countryList, count:countryList.length });
+// }
+
+router.get('/country', async (req, res) => {
+    const countryList = await countryModel.showcountryList();
+    res.render('CountryListView', {data:countryList, count:countryList.length});
+});
+
+// async function showCountryDetailView(req, res) {
+//     try {
+//         const id = req.params.id;
+//         console.log('country: ', id);
+//         const info = await countryModel.detailCountry(id);
+//         res.render('CountryDetailView',{info:info});
+//         // res.send(info);
+        
+//     } 
+//     catch (error) {
+//         console.log('Can not find, 404');
+//         res.status(error.code).send({msg:error.msg});
+//     }
+// }
+
+router.get('/country/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log('country : ', id);
+        const info = await countryModel.detailCountry(id);
+
+        res.render('CountryDetailView', {info:info});
+    }
+    catch ( error ) {
+        console.log('Can not find, 404');
+        res.status(error.code).send({msg:error.msg});
+    }
+});
 
 function showCountryAddView(req, res) {
         res.render('CountryAddView');    
@@ -28,27 +62,13 @@ async function addcountry(req, res) {
     const data = req.body
     try {
         const result = await countryModel.addcountry(data);
-        res.send({msg:'success', data:result});
+        res.render('CheckCompleteView',{msg:'add success'});
+        // res.send({msg:'success', data:result});
     } catch (error) {
+        // res.render('CheckCompleteView',{msg:'add fail'});
         res.status(500).send(error.msg);
     }
 }
-
-async function showCountryDetailView(req, res) {
-    try {
-        const id = req.params.id;
-        console.log('country: ', id);
-        const info = await countryModel.detailCountry(id);
-        res.render('CountryDetailView',{info:info});
-        // res.send(info);
-        
-    } 
-    catch (error) {
-        console.log('Can not find, 404');
-        res.status(error.code).send({msg:error.msg});
-    }
-}
-
 
 async function showCountryEditView(req, res) {
     try {
@@ -56,7 +76,6 @@ async function showCountryEditView(req, res) {
         console.log('country: ', id);
         const info = await countryModel.detailCountry(id);
         res.render('CountryEditView',{info:info});
-        
     } 
     catch (error) {
         console.log('Can not find, 404');
@@ -79,9 +98,8 @@ async function updatecountry(req, res) {
 
     try {
         const result = await countryModel.updatecountry(data);
-        const countryList = countryModel.showcountryList();
-        res.render('CountryListView',{ data:countryList, count:countryList.length });
         // res.send({msg:'Updated country' + data.name, data:result});
+        res.render('CheckCompleteView',{msg:'update success'});
     }
     catch ( error ) {
         console.error(error);
@@ -95,8 +113,7 @@ async function deletecountry(req, res) {
         const id = req.params.id;
         console.log('Deleted country : ', id);
         const result = await countryModel.deletecountry(id);
-        const countryList = countryModel.showcountryList();
-        res.render('CountryListView',{ data:countryList, count:countryList.length });
+        res.render('CheckCompleteView',{msg: 'delete success'});
     }
     catch ( error ) {
         res.status(400).send({error:'Fail'});
